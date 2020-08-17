030aabd1e13831325c5ec0ba1896f5596b9b121c
jane <sphariab@gmail.com>
2020-08-17 15:52:13 +0300

    Updates

---
+++ precache-manifest.025c1efa31066607afeebf0d2d177ffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692d2232acd9850c8c875cdf8bf7345d",
+    "url": "/editable-layout/index.html"
+  },
+  {
+    "revision": "12f0f03124c117ef86da",
+    "url": "/editable-layout/static/css/main.37843d0a.chunk.css"
+  },
+  {
+    "revision": "c79f35208a189e383f76",
+    "url": "/editable-layout/static/js/2.a09a8725.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/editable-layout/static/js/2.a09a8725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f0f03124c117ef86da",
+    "url": "/editable-layout/static/js/main.aa8e2403.chunk.js"
+  },
+  {
+    "revision": "febe079783369776220c",
+    "url": "/editable-layout/static/js/runtime-main.b810a675.js"
+  }
+]);
